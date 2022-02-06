ef953a9a9b4fe4fa3d6696fbd4c5abff4af2e062
izlam <isalm9090@gmail.com>
2022-02-06 23:55:20 +0200

    test1 on live streem

---
+++ app/src/main/java/com/izlam/lifejava/MainActivity.java
@@ -0,0 +1,61 @@
+package com.izlam.lifejava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import io.agora.rtc.Constants;
+
+public class MainActivity extends AppCompatActivity {
+    int channelProfile;
+    public static final String channelMessage = "com.agora.samtan.agorabroadcast.CHANNEL";
+    public static final String profileMessage = "com.agora.samtan.agorabroadcast.PROFILE";
+
+    private String APP_ID="ca80305b9e5943d2b71ca13bdc63c15c";
+    private String STATUS="285be289c73b43df8fe72d1e899edee9";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+        // Here, this is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+        }
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.host:
+                if (checked) {
+                    channelProfile = Constants.CLIENT_ROLE_BROADCASTER;
+                }
+                break;
+            case R.id.audience:
+                if (checked) {
+                    channelProfile = Constants.CLIENT_ROLE_AUDIENCE;
+                }
+                break;
+        }
+    }
+
+    public void onSubmit(View view) {
+        EditText channel = (EditText) findViewById(R.id.channel);
+        String channelName = channel.getText().toString();
+        Intent intent = new Intent(this, VideoActivity.class);
+        intent.putExtra(channelMessage, channelName);
+        intent.putExtra(profileMessage, channelProfile);
+        startActivity(intent);
+    }
+}
